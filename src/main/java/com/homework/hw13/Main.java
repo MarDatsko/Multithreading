12fd9e1affba27743729b977d9d36605a52995fb
MarDatsko <48189965+MarDatsko@users.noreply.github.com>
2020-02-04 15:06:38 +0200

    Delete Main.java

--- src/main/java/com/homework/hw13/Main.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.homework.hw13;
-
-import main.java.com.homework.hw13.Executor;
-
-public class Main {
-    public static void main(String[] args) {
-	    new Executor().run();
-    }
-}
